507aa7b4e6a61204458ced9a6a6b131a412a13dc
Adam Willden <adam.j.willden@gmail.com>
2017-10-20 09:38:25 +0100

    fix(hmr): modules from transpilers do not use the 'css-resource-plugin' (fixes #16)

--- src/aurelia-hot-module-reload.ts
+++ src/aurelia-hot-module-reload.ts
@@ -224,13 +224,16 @@ export class HmrContext {
       return; // first load
     }
 
-    const cssPluginModuleId = this.loader.applyPluginToUrl(moduleId, 'css-resource-plugin');
+    const extensionIndex = moduleId.lastIndexOf('.');
+    const moduleExtension = moduleId.substring(extensionIndex + 1);
+    const pluginName = `${moduleExtension}-resource-plugin`;
+    const cssPluginModuleId = this.loader.applyPluginToUrl(moduleId, pluginName);
 
     console.log(`Handling HMR for ${moduleId}`);
     delete (this.loader.moduleRegistry as any)[moduleId];
     delete (this.loader.moduleRegistry as any)[cssPluginModuleId];
 
-    const analyzedModule = this.moduleAnalyzerCache[`css-resource-plugin!${moduleId}`];
+    const analyzedModule = this.moduleAnalyzerCache[cssPluginModuleId];
     if (!analyzedModule.resources || !analyzedModule.resources.length) {
       console.error(`Something's wrong, no resources for this CSS file ${moduleId}`);
       return;
